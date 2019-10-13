f0839ef228e849c56957a5e7e6ff932673a21cb2
tg0825 <tg0825@gmail.com>
2019-10-13 17:54:55 +0900

    create to crud react project

---
+++ src/Root.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss';
+import Button from './Button';
+
+class Root extends React.Component {
+    render () {
+        return (
+            <div>
+                <Button/>
+            </div>
+        )
+    }
+}
+
+export default Root;
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname + '/build')
+    },
+    devServer: {
+        contentBase: path.resolve('./build'),
+        index: 'index.html',
+        port: 9000
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: "/node_modules",
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            compact: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
