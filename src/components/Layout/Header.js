eb5733931c03abcffb4890acf6af862ea155b211
tg0825 <tg0825@gmail.com>
2020-01-29 03:12:45 +0900

    * 로고 클릭으로 메인 페이지 이동 * 로그인 관련 준비

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default () => {
     return (
         <div className="header">
-            사내맛집
+            <Link to="/">
+                사내맛집
+            </Link>
+            로그인
         </div>
     );
 };
