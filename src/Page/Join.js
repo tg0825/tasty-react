09ace763f5021a6d83f4ad67330ba442b33e6c37
tg0825 <tg0825@gmail.com>
2020-02-22 09:20:20 +0900

    회원가입 작업

--- src/Page/Join.js
+++ src/Page/Join.js
@@ -1,35 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Join = () => {
+    const [formData, setFormData] = useState({});
+
+    /**
+     * input change handler
+     */
+    const handleChange = e => {
+        const { name, value } = e.currentTarget;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+
+        console.log(formData);
+    };
+
+    /**
+     * 전송
+     */
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e);
+    };
+
     return (
-        <form action="" method="post" className="wrap">
+        <form
+            action=""
+            method="post"
+            className="wrap"
+            onSubmit={e => handleSubmit(e)}
+        >
             <div className="input-text">
                 <div>id</div>
                 <div>
-                    <input type="text" />
+                    <input
+                        name="id"
+                        type="text"
+                        onChange={e => handleChange(e)}
+                    />
                 </div>
             </div>
             <div className="input-text">
                 <div>password</div>
                 <div>
-                    <input type="text" />
+                    <input
+                        name="pw"
+                        type="password"
+                        onChange={e => handleChange(e)}
+                    />
                 </div>
             </div>
 
             <div>
                 <div>password</div>
                 <div>
-                    <input type="text" />
+                    <input
+                        name="pw2"
+                        type="password"
+                        onChange={e => handleChange(e)}
+                    />
                 </div>
             </div>
 
             <div className="input-text">
                 <div>name</div>
                 <div>
-                    <input type="text" />
+                    <input
+                        name="name"
+                        type="text"
+                        onChange={e => handleChange(e)}
+                    />
                 </div>
             </div>
+            <button type="submit">전송</button>
         </form>
     );
 };
