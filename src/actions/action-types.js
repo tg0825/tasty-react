2b8e511779ac88854c408f16e95790ce03ac4fe2
tg0825 <tg0825@gmail.com>
2020-03-02 08:28:25 +0900

    불필요 파일 삭제

--- src/actions/action-types.js
+++
@@ -1,5 +0,0 @@
-export const GET_POST = 'GET_POST';
-export const GET_POST_LIST = 'GET_POST_LIST';
-export const ADD_POST = 'ADD_POST';
-export const PATCH_POST = 'PATCH_POST';
-export const DELETE_POST = 'DELETE_POST';
