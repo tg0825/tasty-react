78e5b85b139f40de540206033ecab0de800ab068
tg0825 <tg0825@gmail.com>
2020-03-08 02:01:13 +0900

    css 기초 작업 완료

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Styled from 'styled-components';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
+const StSlider = Styled.div`
+    .img {
+        background: #eee;
+    }
+    
+    .slick-arrow {
+        border: 1px solid #ddd;
+    }
+`;
 
 const Main = () => {
     const settings = {
@@ -13,26 +24,26 @@ const Main = () => {
         slidesToScroll: 1,
     };
     return (
-        <Slider {...settings}>
-            <div>
+        <StSlider as={Slider} {...settings}>
+            <div className="img">
                 <h3>1</h3>
             </div>
-            <div>
+            <div className="img">
                 <h3>2</h3>
             </div>
-            <div>
+            <div className="img">
                 <h3>3</h3>
             </div>
-            <div>
+            <div className="img">
                 <h3>4</h3>
             </div>
-            <div>
+            <div className="img">
                 <h3>5</h3>
             </div>
-            <div>
+            <div className="img">
                 <h3>6</h3>
             </div>
-        </Slider>
+        </StSlider>
     );
 };
 
