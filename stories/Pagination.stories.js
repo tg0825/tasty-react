a4960b2de73d522bf601fabc8969cd6bd04c09df
tg0825 <tg0825@gmail.com>
2020-03-10 02:41:05 +0900

    storybook pagination 추가

--- stories/Pagination.stories.js
+++ stories/Pagination.stories.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 import { action } from '@storybook/addon-actions';
-import Pagination from '../src/components/pagination';
+import Pagination from 'Comp/pagination';
 
 export default {
-    title: 'Pagination',
+    title: 'components|basic/Pagination', // 스토리북에서 보여질 그룹과 경로를 명시
     component: Pagination,
+    excludeStories: /.*Data$/,
 };
 
-export const normal = () => <Pagination />;
-
-normal.story = {
-    name: 'pagination',
+export const paginationData = {
+    headers: {
+        'x-total-count': 100,
+    },
 };
+
+export const actionsData = {
+    onClickPageBtn: action('click pagination btn'),
+};
+
+export const normal = () => (
+    <Pagination {...actionsData} paginationData={{ ...paginationData }} />
+);
